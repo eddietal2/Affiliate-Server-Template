e0ebcc47e8e7f96a43ed010058c9b82e4283d50a
Eddie <eddielacrosse2@gmail.com>
2021-11-23 14:21:24 -0500

    Nodemon; Test

--- server.js
+++ server.js
@@ -9,26 +9,26 @@ const dotenv                  = require('dotenv');
 dotenv.config();
 
 // Connect to Mongo via Mongoose
-mongoose
+// mongoose
   // For DeprecationWarning:  collection.ensureIndex is deprecated.  Use createIndexes instead.
 
-  .set('useCreateIndex', true)
-  .set('useFindAndModify', false)
+  // .set('useCreateIndex', true)
+  // .set('useFindAndModify', false)
 
-  .connect(process.env.DB_HOST_DEV, {
-    useNewUrlParser: true, useUnifiedTopology: true
-  })
+  // .connect(process.env.DB_HOST_DEV, {
+  //   useNewUrlParser: true, useUnifiedTopology: true
+  // })
 
-  .then(() => console.log('Connected to MongoDB'))
+  // .then(() => console.log('Connected to MongoDB'))
 
-  .catch(err => console.log(err))
+  // .catch(err => console.log(err))
 
 // Middleware
 app.use(cors());
 app.use(express.json())
 
 // API Routes
-app.use()
+// app.use()
 
 // Listen on PORT
 const port = process.env.PORT || 3000;
